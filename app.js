6eb006c8776e4f508a0f5b1eb493656e3e33ae0e
Henry Ryu <hkryucr@gmail.com>
2020-09-20 11:32:29 -0700

    format for eslint - 2

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
-// const http = require('http').createServer(app);
-// const io = require('socket.io')(http);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
@@ -35,9 +35,9 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
-// io.on("connect", socket => {
-//   console.log("connect")
-// })
+io.on("connect", socket => {
+  console.log("connect")
+})
 
-// http.listen(port)
+http.listen(port)
 

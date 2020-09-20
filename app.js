c9c01da492a39411012d1aacea93fbfa91436081
Henry Ryu <hkryucr@gmail.com>
2020-09-20 11:31:47 -0700

    format for eslint

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const users = require("./routes/api/users");
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const users = require('./routes/api/users');
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
 
-const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI;
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -17,7 +17,7 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => {
-    console.log("Connected to mongoDB");
+    console.log('Connected to mongoDB');
   })
   .catch(err => console.log(err));
 
@@ -35,9 +35,9 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
-io.on("connect", socket => {
-  console.log("connect")
-})
+// io.on("connect", socket => {
+//   console.log("connect")
+// })
 
-http.listen(port)
+// http.listen(port)
 

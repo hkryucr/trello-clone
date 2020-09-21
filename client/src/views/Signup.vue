6715a66519ab7f955312668312147519fc08807a
mckgegis <mckgegis@yahoo.com>
2020-09-21 13:55:50 -0700

    fixed css on signup form

--- client/src/views/Signup.vue
+++ client/src/views/Signup.vue
@@ -3,24 +3,33 @@
     <img alt="Trello" class="trello-main-logo" src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/76ceb1faa939ede03abacb6efacdde16/trello-logo-blue.svg">
     <div class="form-container">
       <form>
-        <h1>Sign up for your account</h1>
+        <h1 class="signup-header">Sign up for your account</h1>
         <input class="signup-input" type="text" name="email" id="email" autocorrect="off" spellcheck="false" autocapitalize="off" autofocus="autofocus" placeholder="Enter email" value="" autocomplete="username" inputmode="email">
-        <div>
+        <p class="signup-policy" style="text-align: start">
           By signing up, you confirm that you've read and accepted our
-          <a href="">Terms of Service</a>
+          <a class="policy-link">Terms of Service</a>
           and
-          <a href="">Privacy Policy</a>
+          <a class="policy-link">Privacy Policy</a>
           .
-        </div>
-        <button>Continue</button>
+        </p>
+        <input type="submit" class="signup-submit-button" value="Continue" disabled/>
       </form>
-      <div>OR</div>
-      <div class="signup-links">
-        <a class="signup-link" href="">Continue with Google</a>
-        <a class="signup-link" href="">Continue with Microsoft</a>
-        <a class="signup-link" href="">Continue with Apple</a>
+      <div class="signup-or">OR</div>
+      <div class="signup-link-container">
+        <div class="signup-link">
+          <span id="google-icon"></span>
+          <span class="signup-link-text">Continue with Google</span>
+        </div>
+        <div class="signup-link">
+          <span id="microsoft-icon"></span>
+          <span class="signup-link-text">Continue with Microsoft</span>
+        </div>
+        <div class="signup-link">
+          <span id="apple-icon"></span>
+          <span class="signup-link-text">Continue with Apple</span>
+        </div>
       </div>
-      <a href="">Already have an account? Log In</a>
+      <a class="login-link" href="/login">Already have an account? Log In</a>
     </div>
   </div>
 </template>
@@ -39,8 +48,9 @@
         border-radius: 3px;
         padding: 25px 40px;
         box-shadow: rgba(0,0,0,0.1) 0 0 10px;
-        width: 385px;
-        align-self: center;
+        width: 400px;
+        height: 538px;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
     }
     .signup-page{
       display: flex;
@@ -48,15 +58,28 @@
       align-items: center;
     }
     .signup-input{
+      font-size: 14px;
+      width: 100%;
+      background-color: #FAFBFC;
       border: 2px solid #DFE1E6;
-      width: 75%;
+      -moz-box-sizing: border-box;
+      box-sizing: border-box;
+      border-radius: 3px;
+      height: 44px;
       padding: 7px;
+      -webkit-transition: background-color .2s ease-in-out 0s,border-color .2s ease-in-out 0s;
+      transition: background-color .2s ease-in-out 0s,border-color .2s ease-in-out 0s;
     }
-    .signup-links{
+    .signup-link-container{
       display: flex;
       flex-direction: column;
+      padding-bottom: 10px;
+      border-bottom: 1px solid lightgrey;
     }
     .signup-link{
+      display: flex;
+      align-items: center;
+      justify-content: center;
       background: #fff;
       color: rgba(0,0,0,0.54);
       box-shadow: rgba(0,0,0,0.2) 1px 1px 5px 0;
@@ -67,5 +90,105 @@
       height: 39px;
       padding-top: 0;
       margin-bottom: 12px;
+      -webkit-transition: background-color .2s ease-in-out 0s,border-color .2s ease-in-out 0s;
+      transition: background-color .2s ease-in-out 0s,border-color .2s ease-in-out 0s;
+    }
+
+    .signup-header{
+      text-align: center;
+      color: #5E6C84;
+      font-size: 16px;
+      letter-spacing: -0.01em;
+      line-height: 28px;
+      margin-top: 10px;
+      margin-bottom: 25px;
+      font-weight: bold;
+    }
+
+    .signup-policy{
+      text-align: start;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      font-size: 12px;
+      line-height: 16px;
+      color: #5E6C84;
+      font-weight: 349;
+      font-family: "-apple-system",BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,"Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+    }
+
+    .signup-submit-button{
+      background: #E2E4E6;
+      border-radius: .3em;
+      color: hsl(0,0%,55%);
+      display: inline-block;
+      font-weight: bold;
+      margin-bottom: .8em;
+      padding: .6em 1.3em;
+      position: relative;
+      text-decoration: none;
+      border: 0px;
+      width: 100%;
+      cursor: default;
+      font-family: Arial, Helvetica, sans-serif;
+    }
+
+    .signup-or{
+      text-align: center;
+      font-size: 12px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+    }
+
+    #google-icon{
+      background: url('../assets/google.png');
+      display: inline-block;
+      border-radius: 1px;
+      width: 18px;
+      height: 18px;
+      vertical-align: middle;
+    }
+
+    #microsoft-icon{
+      background: url('../assets/microsoft.png');
+      display: inline-block;
+      border-radius: 1px;
+      width: 18px;
+      height: 18px;
+      vertical-align: middle;
+    }
+
+    #apple-icon{
+      background: url('../assets/apple.png');
+      display: inline-block;
+      border-radius: 1px;
+      width: 18px;
+      height: 18px;
+      vertical-align: middle;
+    }
+
+    .signup-link-text{
+      font-size: 14px;
+      font-family: inherit;
+      font-weight: bold;
+      color: #505f79;
+      padding-left: .5em;
+      height: 32px;
+      line-height: 32px;
+    }
+
+    .login-link{
+      display: block;
+      padding: 20px;
+      text-decoration: none;
+      color: blue;
+    }
+
+    .policy-link{
+      text-decoration: none;
+      color: blue;
+    }
+
+    .login-link:hover, .policy-link:hover{
+      text-decoration: underline;
     }
   </style>

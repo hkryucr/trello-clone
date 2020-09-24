cd927c3388a1a258f8c02f9a20a7c359d47eb09e
anson <ansonchongwus44@gmail.com>
2020-09-24 16:52:40 -0700

    add create task handler

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -1,4 +1,6 @@
-const Task = ("../models/Task")
+const Column = require("../models/Column");
+const User = require("../models/User");
+const Task = require("../models/Task")
 
 class TaskController {
   async moveTask(io, socket, data) {
@@ -31,7 +33,37 @@ class TaskController {
   }
 
   async createTask(io, socket, data) {
-    // 
+    /*
+    data: {
+      name,
+      description
+      user: objectid
+      column: objectid,
+    },
+    update the task array in column
+    */
+    const { name, description, userId, columnId } = data;
+    const user = await User.findById({userId});
+    const column = await Column.findById({columnId});
+    if (!user) {
+      socket.emit("error", "user not found!");
+      return 
+    }
+    const task = new Task({
+      name, 
+      description, 
+      creator: userId,
+      column: columnId
+    })
+
+    task.save().then( task => {
+      column.tasks.push(task._id);
+      column.save().then( column => {
+        io.socket.emit("newTask", task);
+      }).catch( err => {
+        socket.emit("error", err);
+      })
+    })
     return
   }
 

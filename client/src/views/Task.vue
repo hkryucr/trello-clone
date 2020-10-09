6dd3a534f8eb333d781faffad95180d09be0fabd
anson <ansonchongwus44@gmail.com>
2020-10-08 21:57:37 -0700

    add css

--- client/src/views/Task.vue
+++ client/src/views/Task.vue
@@ -29,7 +29,7 @@
             <div>
               in the list
               <router-link
-                  class="text-gray-link"
+                class="text-gray-link"
                 :to="{ name: 'board', params: { id: this.$route.params.id } }"
                 >{{ column.name }}</router-link
               >
@@ -37,30 +37,51 @@
           </div>
         </div>
       </div>
-      <div class="task-main flex flex-row flex-grow items-start justify-between w-full">
+      <div
+        class="task-main flex flex-row flex-grow items-start justify-between w-full"
+      >
         <div class="w-4/5">
           <div class="task-description relative">
-            <span class="task-description-icon icon-description icon-lg window-module-title-icon absolute"></span>
-            <h3>description</h3>
+            <span
+              class="task-description-icon icon-description icon-lg window-module-title-icon absolute"
+            ></span>
+            <h3 class="pt-1">description</h3>
             <textarea
               ref="taskDescription"
-              class="relative bg-trransparent w-full px-2 border mt-2 h-64 border-none leading-normal"
+              class="task-description-input relative overflow-hidden bg-trransparent w-full px-2 border mt-2 h-64 border-none leading-normal resize-none"
               :value="task.description"
               @change="updateTaskProperty($event, 'description')"
             />
             <input
+              class="primary"
               type="submit"
               value="Save"
               @click.prevent="updateTaskDescription($event)"
             />
+            <router-link
+              class="text-gray-link icon-lg icon-close dark-hover cancel js-cancel-edit"
+              :to="{ name: 'board', params: { id: this.$route.params.id } }"
+            ></router-link>
           </div>
         </div>
         <div class="task-view-right w-1/5">
           <h3>Actions</h3>
           <ul class="button-list p-1">
-            <li class="flex flex-row"><button><span class="icon-sm icon-move"></span>Move</button></li>
-            <li class="flex flex-row"><button><span class="icon-sm icon-copy"></span>Copy</button></li>
-            <li class="flex flex-row"><button><span class="icon-sm icon-archive"></span>Delete</button></li>
+            <li class="flex flex-row">
+              <button class="button-link">
+                <span class="icon-sm icon-move"></span>Move
+              </button>
+            </li>
+            <li class="flex flex-row">
+              <button class="button-link">
+                <span class="icon-sm icon-copy"></span>Copy
+              </button>
+            </li>
+            <li class="flex flex-row">
+              <button class="button-link">
+                <span class="icon-sm icon-archive"></span>Delete
+              </button>
+            </li>
           </ul>
         </div>
       </div>
@@ -137,18 +158,18 @@ export default {
 }
 .task-main-header {
   padding: 3px 0;
-  white-space:nowrap;
-  display:inline-block;
+  white-space: nowrap;
+  display: inline-block;
 }
 .task-main-header > input {
   @apply rounded;
   height: 100%;
   width: fit-content;
   max-width: 100%;
-  box-sizing:border-box;
+  box-sizing: border-box;
   outline: transparent;
   font-weight: bolder;
-  white-space:nowrap;
+  white-space: nowrap;
 }
 .task-name {
   padding: 0;
@@ -162,16 +183,71 @@ export default {
 .task-description-icon {
   left: -40px;
 }
-.task-description {
-  padding: 8px 0;
-}
 .task-main {
   padding: 0 8px 8px 16px;
 }
 .task-description {
   margin: 0 0 4px 40px;
 }
+.task-description-input {
+  min-height: 108px;
+  overflow-wrap: break-word;
+  border-radius: 3px;
+}
+.task-description-input:focus {
+  box-shadow: inset 0 0 0 2px #0079bf;
+}
+.task-main > div:first-child {
+  padding: 0 8px 8px 0;
+}
 .button-list {
   list-style-type: none;
+}
+input[type="submit"].primary {
+  background-color: #5aac44;
+  box-shadow: none;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 20px;
+  margin: 8px 4px 0 0;
+  padding: 6px 12px;
+  text-align: center;
+  border-radius: 3px;
+}
+input[type="submit"].primary:hover {
+  background-color: #61bd4f;
+  box-shadow: none;
+  border: none;
+  color: #fff;
+}
+
+.button-link {
+  background-color: rgba(9, 30, 66, 0.04);
+  box-shadow: none;
+  border: none;
+  border-radius: 3px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: block;
+  height: 32px;
+  color: #172b4d;
+  width: 100%;
+  text-align: left;
+  margin-top: 8px;
+  max-width: 300px;
+  overflow: hidden;
+  padding: 6px 12px;
+  position: relative;
+  text-decoration: none;
+  text-overflow: ellipsis;
+  -webkit-user-select: none;
+  user-select: none;
+  white-space: nowrap;
+  transition-property: background-color, border-color, box-shadow;
+  transition-duration: 85ms;
+  transition-timing-function: ease;
 }
 </style>

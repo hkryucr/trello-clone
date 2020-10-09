f56475f3e0804a702d1d98c50b8f3d4328ebc88d
jordantom <jordan.tom88@gmail.com>
2020-10-08 21:29:27 -0700

    Finish HTML for boards page

--- client/src/views/Boards.vue
+++ client/src/views/Boards.vue
@@ -8,7 +8,7 @@
               <li class="user-boards">
                 <a class="boards-link" href="">
                   <span class="icon-container">
-                    <span class="icon-link"></span>
+                    <span class="icon-board icon-sm _2aV_KY1gTq1qWc"></span>
                   </span>
                   <span class="boards-link-text">Boards</span>
                 </a>
@@ -17,7 +17,7 @@
                 <li class="templates">
                   <a class="templates-link" href="">
                     <span class="icon-container">
-                      <span class="icon-link"></span>
+                      <span class="icon-template-board icon-sm _2aV_KY1gTq1qWc"></span>
                     </span>
                     <span class="templates-link-text">Templates</span>
                   </a>
@@ -27,7 +27,7 @@
                 <a class="homepage-link" href="">
                   <span class="icon-container">
                     <span class="icon-wrapper">
-                      <span class="icon-link"></span>
+                      <span class="icon-home icon-sm _2aV_KY1gTq1qWc"></span>
                     </span>
                   </span>
                   <span class="homepage-link-text">Home</span>
@@ -35,26 +35,43 @@
               </li>
             </ul>
           </div>
+          <div class="create-team-nav">
+            <div class="create-team-nav-header">
+              <div class="NC6qaILF7dGKjb">
+                <div class="_2zEdWKjwDvxZHR dG8NJxS20S4HJ2">
+                  <div class="_mtkwfAlvk6O3f">TEAMS</div>
+                </div>
+              </div>
+            </div>
+            <div class="create-team">
+              <button class="_33CvMKqfH4Yf0j _3SBHBJq0AAxzqg" data-test-id="home-navigation-create-team-button" aria-label="Create a team">
+                <span class="_12-5x14JSgUact">
+                  <span class="_2aV_KY1gTq1qWc">
+                    <span class="icon-add icon-sm _2aV_KY1gTq1qWc"></span>
+                  </span>
+                </span>
+                <span class="_3qwe2tMMFonNvf">Create a team</span>
+              </button>
+            </div>
+          </div>
         </nav>
         <div class="my-boards">
           <div class="my-boards-section-header">
             <div class="boards-page-icon">
-              <span></span>
+              <span class="icon-lg icon-member"></span>
             </div>
             <h3 class="boards-page-header-name">Personal Boards</h3>
           </div>
           <div class="boards-container">
             <div class="boards-index">
-              <div class="boards-index-item">
-                <a class="board-tile" href="">
-                  <span class="board-tile-fade"></span>
-                  <div class="board-tile-details">
-                    <div class="board-tile-details-name" v-for="(board, $boardIndex) of boards" :key="$boardIndex">
-                      {{board.name}}
-                    </div>
+              <a class="board-tile" href="">
+                <span class="board-tile-fade"></span>
+                <div class="board-tile-details">
+                  <div class="board-tile-details-name" v-for="(board, $boardIndex) of boards" :key="$boardIndex">
+                    {{board.name}}
                   </div>
-                </a>
-              </div>
+                </div>
+              </a>
             </div>
           </div>
         </div>
@@ -119,6 +136,7 @@ export default {
 .home-content-wrapper {
   position: sticky;
   top: 0px;
+  display: flex;
 }
 .home-left-sidebar-container {
   margin: 40px 0 0;
@@ -215,5 +233,93 @@ export default {
 .homepage-link-text {
   font-size: 14px;
   color: #172b4d;
+}
+.create-team-nav {
+  display: block;
+}
+.NC6qaILF7dGKjb {
+  display: flex;
+}
+.dG8NJxS20S4HJ2 {
+  margin: 0;
+  align-items: baseline;
+  display: flex;
+  height: 32px;
+  justify-content: flex-start;
+  margin-bottom: 8px;
+  padding: 0 0 0 8px;
+}
+._mtkwfAlvk6O3f {
+  color: #5e6c84;
+  font-size: 12px;
+  font-weight: 500;
+  letter-spacing: .04em;
+  line-height: 16px;
+  margin-top: 16px;
+  text-transform: uppercase;
+  flex: 1 1 auto;
+  margin: 0;
+  padding: 8px 0;
+}
+._3qwe2tMMFonNvf {
+  flex: 1 1 auto;
+  font-size: 14px;
+}
+._3SBHBJq0AAxzqg {
+  height: auto;
+  width: 100%;
+  padding: 6px 8px 6px 0;
+  box-shadow: none;
+  font-weight: 400;
+  color: rgba(9,30,66,.66);
+}
+._33CvMKqfH4Yf0j {
+  margin: 0;
+  margin-top: 6px;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  background-color: transparent;
+  border-radius: 4px;
+  display: flex;
+  overflow: hidden;
+  text-decoration: none;
+  text-decoration: none;
+  transition-property: background-color,border-color,box-shadow;
+  transition-duration: 85ms;
+  transition-timing-function: ease;
+  cursor: pointer;
+  text-align: left;
+}
+.my-boards-section-header {
+  display: flex;
+}
+.icon-lg {
+  color: #42526e;
+  height: 32px;
+  line-height: 32px;
+  width: 32px;
+}
+.boards-page-header-name {
+  display: inline-block;
+  line-height: 24px;
+  margin: 4px 0 0;
+  font-size: 16px;
+  font-weight: 700;
+  flex: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.boards-index {
+  display: flex;
+}
+.board-tile-fade {
+  /* background-color: rgba(0,0,0,.15); */
+  bottom: 0;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
 }
 </style>

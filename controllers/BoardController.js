92a75bf41fdd6a1187166b542bf1523ac69486f1
mckgegis <mckgegis@yahoo.com>
2020-10-31 12:23:07 -0700

    fixed create board modal enter function and added newboard to users starreboards

--- controllers/BoardController.js
+++ controllers/BoardController.js
@@ -10,6 +10,21 @@ class BoardController {
     const user = await User.findById(data.user)
     board.save().then(board => {
       user.boards.push(board._id)
+      const query = { _id: board.user };
+
+      let sb = `starredBoards.${board._id}`;
+
+      User.findByIdAndUpdate(query, {
+        $set: {
+          [sb]: false
+        }
+      }, { new: true },
+        function (err, doc) {
+          io.sockets.emit("UPDATE_USER_STARRED_BOARDS", {
+            boardId: board._id,
+            bool: false
+          });
+        })
       user.save()
       .then(user => {
         io.sockets.emit("CREATED_BOARD", board)

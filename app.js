1d2bd663ef8be006fadb0293f808449426c12ff3
Henry Ryu <hkryucr@gmail.com>
2020-11-03 10:01:18 -0800

    changed target dist

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ mongoose
 
 // Setup the routers
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/dist"));
+  app.use(express.static("dist"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {

863cd88a9afca778f4dd6b167e4c7613e5885280
Henry Ryu <hkryucr@gmail.com>
2020-11-03 11:37:30 -0800

    update socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,19 +1,17 @@
 // import config from './config'
+import pkg from '../package.json'
 const io = require('socket.io-client')
 
-// console.log(process.env.PORT)
-// console.log(process.env)
 // let socket = io(config[process.env.NODE_ENV].endpoint)
 
-const port = process.env.VUE_APP_SOCKET_SERVER_URL
-let socket = io(port)
+const port = window.location.hostname
+let socket
 
-// if (process.env.NODE_ENV == "production") {
-//   console.log("production")
-// } else if (process.env.NODE_ENV == "development") {
-//   // socket = io(`/`);
-//   console.log(process.env)
-// }
+if (process.env.NODE_ENV === 'production') {
+  socket = io(port)
+} else if (process.env.NODE_ENV === 'development') {
+  socket = io(pkg.porxy)
+}
 
 socket.on('connect', () => {
   socket.send('Hello!')

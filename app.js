97af9cb709908edf3271063f176348ecdc17910a
anson <ansonchongwus44@gmail.com>
2020-11-03 12:58:43 -0800

    prod

--- app.js
+++ app.js
@@ -55,17 +55,18 @@ app.use("/api/backgrounds", backgrounds);
 
 // WEBSOCKET CONFIGURATION
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-        "Access-Control-Allow-Credentials": true
-    };
-    res.writeHead(200, headers);
-    res.end();
-  }
-});
+// const io = require("socket.io")(http, {
+//   handlePreflightRequest: (req, res) => {
+//     const headers = {
+//         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+//         "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+//         "Access-Control-Allow-Credentials": true
+//     };
+//     res.writeHead(200, headers);
+//     res.end();
+//   }
+// });
+const io = require("socket.io")(http)
 // io.set('origins', '*:*');
 io.on("connection", (socket) => {
   socket.on("updateBoard", async (data) => {

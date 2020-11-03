6cf7ecb3e34e69e8169b15f4e8141419cda05e97
Henry Ryu <hkryucr@gmail.com>
2020-11-03 01:18:23 -0800

    fixed the route

--- app.js
+++ app.js
@@ -37,9 +37,9 @@ mongoose
 
 // Setup the routers
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {

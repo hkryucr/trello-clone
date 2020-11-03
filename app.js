27ab3248edd0683e3f6c88837e0ad80d6f3ad7f0
Henry Ryu <hkryucr@gmail.com>
2020-11-02 23:54:49 -0800

    before deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const users = require("./routes/api/users");
 const boards = require("./routes/api/boards");
@@ -36,7 +37,7 @@ mongoose
 
 // Setup the routers
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("frontend/build"));
+  app.use(express.static("client/build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });

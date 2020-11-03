1bb5bdd8fc228272df879942b82b5be3cad9ce77
Henry Ryu <hkryucr@gmail.com>
2020-11-03 11:16:31 -0800

    fix app.js6

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ mongoose
 
 // Setup the routers
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("dist"));
+  app.use(express.static(__dirname, "dist"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });

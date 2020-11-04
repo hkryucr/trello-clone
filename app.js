958941c59085913a339b00da90e6e326f01159ba
anson <ansonchongwus44@gmail.com>
2020-11-03 22:47:09 -0800

    testing

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Setup the routers
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, "dist"));
+  app.use(express.static(__dirname, 'dist'));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "dist", "index.html"));
   });
